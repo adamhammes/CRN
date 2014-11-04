4254d78922efaea0bf69bcae405bb8e039eebbc5
Adam Hammes <adamhammes@centetune.com>
2014-11-04 11:24:04 -0600

    Actually called main, fixed typo in constructor

--- Test.py
+++ Test.py
@@ -1,6 +1,9 @@
 from CRN import CRN
 
 def main():
-  crn = CRN( gui_text = 'testfile.txt')
+  crn = CRN( gui_txt = 'testfile.txt')
   crn.crn_print( file_name = 'crn_output.txt')
   crn.diff_eq_print( file_name = 'diff_eq_output.txt')
+
+if __name__ == '__main__':
+	main()
