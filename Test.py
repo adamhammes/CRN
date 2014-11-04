a21c44431d5c94f26b8afe75b4104ac62882bdd5
Adam Hammes <adamhammes@centetune.com>
2014-11-04 11:17:22 -0600

    need to specify argument names

--- Test.py
+++ Test.py
@@ -2,5 +2,5 @@ from CRN import CRN
 
 def main():
   crn = CRN( gui_text = 'testfile.txt')
-  crn.crn_print('crn_output.txt')
-  crn.diff_eq_print('diff_eq_output.txt')
+  crn.crn_print( file_name = 'crn_output.txt')
+  crn.diff_eq_print( file_name = 'diff_eq_output.txt')
