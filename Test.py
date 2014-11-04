5ff99ebce7354491e688ce41391dade480bb7029
Adam Hammes <adamhammes@centetune.com>
2014-11-04 11:11:16 -0600

    Specified constructor arguments

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 from CRN import CRN
 
 def main():
-  crn = CRN('testfile.txt')
+  crn = CRN( gui_text = 'testfile.txt')
   crn.crn_print('crn_output.txt')
   crn.diff_eq_print('diff_eq_output.txt')
