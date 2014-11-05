7dc0537dba48eb916aeb951564a1c501b28bbfa2
Adam Hammes <adamhammes@centetune.com>
2014-11-05 14:50:12 -0600

    Moved file io exception handling to CRN.py

--- Test.py
+++ Test.py
@@ -6,8 +6,6 @@ def main():
 		crn = CRN('testfile.txt')
 		crn.crn_print('crn_output.txt')
 		crn.diff_eq_print('diff_eq_output.txt')
-	except IOError as e:
-		print('IOError: ' + str(e))
 	except FileFormatError as e:
 		print('FileFormatError: ' + str(e))
 
