a1ef05f31e8e19a549f7574de122f6ebce1c9aa0
nasutton <nasutton@iastate.edu>
2014-11-04 18:25:43 -0600

    Update Test.py
    Wrapped functionality in a try block which catches IOErrors from file operations and a custom FileFormatError from the CRN class.

--- Test.py
+++ Test.py
@@ -1,9 +1,15 @@
 from CRN import CRN
+from Errors import FileFormatError
 
 def main():
-  crn = CRN( gui_txt = 'testfile.txt')
-  crn.crn_print( file_name = 'crn_output.txt')
-  crn.diff_eq_print( file_name = 'diff_eq_output.txt')
+	try:
+		crn = CRN('testfile.txt')
+		crn.crn_print('crn_output.txt', False)
+		crn.diff_eq_print('diff_eq_output.txt', False)
+	except IOError as e:
+		print('IOError: ' + str(e))
+	except FileFormatError as e:
+		print('FileFormatError: ' + str(e))
 
 if __name__ == '__main__':
 	main()
