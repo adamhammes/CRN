6356488f558cbb3bdde836ad1b6619931e193934
Adam Hammes <adamhammes@centetune.com>
2014-12-03 05:38:35 -0600

    Thought there was an issue, bad equals sign instead

--- Test.py
+++ Test.py
@@ -1,13 +1,17 @@
+# -*- coding: utf-8 -*- 
+
 from CRN import CRN
 from Errors import FileFormatError
 
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def main():
-	try:
-		crn = CRN( eq_text = 'equations.txt' )
-		crn.crn_print( file_name = 'crn_output.txt' )
-	except FileFormatError as e:
-		print('FileFormatError: ' + str(e) )
+	string = [u"x' = − 2αx^2v + 2γz^4", u"y' = 3αx^2v − 3βy^3v^2", u"z' = 4βy^3v^2 − 4γz^4", "v' = αx2^v − 2βy^3v^2 + γz^4"]
+	
+	crn = CRN( eq_text = string)
+
 
 if __name__ == '__main__':
 	main()
